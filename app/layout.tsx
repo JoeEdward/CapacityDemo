bb4ff4236bcb04ed36a22ce079787f34d2020542
Joe <maddo4.dev@gmail.com>
2026-09-22 20:20:36 +0100

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Noto_Sans, Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import {ThemeProvider} from "@/components/theme-provider";
+import {ThemeSwitcher} from "@/components/theme-switcher";
+
+const robotoHeading = Roboto({subsets:['latin'],variable:'--font-heading'});
+
+const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +26,29 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <>
+        <ThemeProvider attribute="class"
+                       defaultTheme="system"
+                       enableSystem
+                       disableTransitionOnChange>
+              <header className={cn('h-15 text-white items-center flex bg-primary shadow-xl')}>
+                <div className={cn('container mx-auto flex justify-between items-center px-2')}>
+                  <h1 className={cn('font-semibold text-xl')}>Capacity</h1>
+                    <ThemeSwitcher />
+                </div>
+              </header>
+              <html
+                  lang="en"
+                  className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, robotoHeading.variable)}
+              >
+
+              <body className="min-h-full flex flex-col">
+                <div className={cn('container mx-auto pt-2')}>
+                  {children}
+                </div>
+              </body>
+              </html>
+        </ThemeProvider>
+    </>
   );
 }
